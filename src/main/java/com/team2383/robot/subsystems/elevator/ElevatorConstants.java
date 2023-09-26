bf8f5cd83b6ee87dc34780e9cc622dc93cb39ca6
Henry LeCompte <henry@lunar-glass.com>
2023-09-25 22:12:52 -0400

    Use wpilib ElevatorSim vs raw LinearSystem

--- src/main/java/com/team2383/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/com/team2383/robot/subsystems/elevator/ElevatorConstants.java
@@ -7,18 +7,18 @@ public class ElevatorConstants {
     public static final double kV = 2.83;
     public static final double kA = 0.07;
 
-    public static final double kP = 4;
-    public static final double kI = 0.0;
-    public static final double kD = 2;
+    public static final double kP = 12;
+    public static final double kI = 1.2;
+    public static final double kD = 4;
 
     public static final double kMaxVoltage = 12.0;
     public static final double kMaxVelocity = 5;
-    public static final double kMaxAcceleration = 3;
+    public static final double kMaxAcceleration = 7;
 
     public static final double kMaxPosition = Units.inchesToMeters(45);
     public static final double kMinPosition = 0.0;
 
-    public static final double kPositionTolerance = 0.0001;
+    public static final double kPositionTolerance = 0.01;
 
     public static final double kEncoderMetersPerRev = 0.5;
 }
--- src/main/java/com/team2383/robot/subsystems/elevator/ElevatorIOSim.java
+++ src/main/java/com/team2383/robot/subsystems/elevator/ElevatorIOSim.java
@@ -1,34 +1,33 @@
 package com.team2383.robot.subsystems.elevator;
 
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.system.LinearSystem;
-import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 
 public class ElevatorIOSim implements ElevatorIO {
-    private final LinearSystem<N1, N1, N1> m_motor;
-    private Matrix<N1, N1> m_X = new Matrix<>(N1.instance, N1.instance);
+    private final DCMotor m_motorModel = DCMotor.getFalcon500(2);
+    private final ElevatorSim m_sim = new ElevatorSim(m_motorModel, 7.75, Units.lbsToKilograms(3),
+            Units.inchesToMeters(2), 0, Units.inchesToMeters(45), true, null);
 
     private double volts;
     private double extension = 0;
 
     public ElevatorIOSim() {
-        m_motor = LinearSystemId.identifyVelocitySystem(ElevatorConstants.kV, ElevatorConstants.kA);
     }
 
     public void updateInputs(ElevatorIOInputs inputs) {
-        m_X = m_motor.calculateX(m_X, VecBuilder.fill(volts), 0.02);
+        m_sim.update(0.02);
 
-        extension += m_X.get(0, 0) * 0.02;
+        extension = m_sim.getPositionMeters();
 
-        inputs.velocityMPS = m_X.get(0, 0);
+        inputs.velocityMPS = m_sim.getVelocityMetersPerSecond();
 
         inputs.positionM = extension;
         inputs.appliedVolts = volts;
     }
 
     public void setVoltage(double voltage) {
+        m_sim.setInputVoltage(voltage);
         volts = voltage;
     }
 
