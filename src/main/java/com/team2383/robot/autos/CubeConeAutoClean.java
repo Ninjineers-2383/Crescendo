90c0fa266ba8b49005355c551d0aaee03ebd0330
Shxl2 <pl238412@ahschool.com>
2023-10-07 13:49:54 -0400

    Added many autos (no workie)

--- src/main/java/com/team2383/robot/autos/CubeConeAutoClean.java
+++ src/main/java/com/team2383/robot/autos/CubeConeAutoClean.java
@@ -11,8 +11,8 @@ import com.team2383.robot.subsystems.wrist.WristSubsystem;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class CubeMobilityAuto extends SequentialCommandGroup {
-    public CubeMobilityAuto(DrivetrainSubsystem drivetrain, ElevatorSubsystem elevator, WristSubsystem wrist,
+public class CubeConeAutoClean extends SequentialCommandGroup {
+    public CubeConeAutoClean(DrivetrainSubsystem drivetrain, ElevatorSubsystem elevator, WristSubsystem wrist,
             FeederSubsystem feeder, String pathName, SwerveAutoBuilder autoBuilder) {
 
         addCommands(
@@ -21,6 +21,9 @@ public class CubeMobilityAuto extends SequentialCommandGroup {
                 new BlizzardCommand(elevator, wrist, BlizzardPresets.CONE_CHUTE).withTimeout(2),
                 new BlizzardCommand(elevator, wrist, BlizzardPresets.GROUND_INTAKE).withTimeout(2),
                 new FeederVoltageCommand(feeder, () -> 0.2).withTimeout(0.5),
-                new FullAutoCommand(drivetrain, pathName, autoBuilder));
+                new FullAutoCommand(drivetrain, pathName, autoBuilder),
+                new BlizzardCommand(elevator, wrist, BlizzardPresets.HIGH).withTimeout(2),
+                new FeederVoltageCommand(feeder, () -> -0.2).withTimeout(0.5));
+
     }
 }
