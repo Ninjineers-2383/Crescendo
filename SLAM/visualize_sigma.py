Fix covariance matrix to enable mapping Send covariance matrix over network tables covariance visualizer

SLAM/visualize_sigma.py
@@ -4,7 +4,7 @@ import numpy as np
 
 if __name__ == "__main__":
     inst = ntcore.NetworkTableInstance.getDefault()
-    table = inst.getTable("AdvantageKit").getSubTable("RealOutputs").getSubTable("SLAM")
+    table = inst.getTable("SLAM")
     xSub = table.getDoubleArrayTopic("sigma").subscribe([0] * (63 * 63))
     inst.startClient4("example client")
     inst.setServer("127.0.0.1", 0)
@@ -14,7 +14,9 @@ if __name__ == "__main__":
     while True:
         val = xSub.get()
         val = np.matrix(val).reshape((63, 63))
-        ax.matshow(val, cmap=plt.cm.Blues)
+        plot = ax.matshow(val, cmap=plt.cm.Blues)
+        cb = fig.colorbar(plot)
         fig.canvas.draw()
         plt.pause(0.01)
         fig.canvas.flush_events()
+        cb.remove()