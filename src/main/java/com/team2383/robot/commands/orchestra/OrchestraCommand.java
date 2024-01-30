748d253797db33f4358f1ec9297dffd7492b8d18
Henry LeCompte <henry@lunar-glass.com>
2024-01-30 09:27:43 -0500

    Fix orchestra command hopefully

--- src/main/java/com/team2383/robot/commands/orchestra/OrchestraCommand.java
+++ src/main/java/com/team2383/robot/commands/orchestra/OrchestraCommand.java
@@ -3,14 +3,16 @@ package com.team2383.robot.commands.orchestra;
 import com.team2383.robot.subsystems.orchestra.OrchestraContainer;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class OrchestraCommand extends Command {
     private OrchestraContainer orchestra = OrchestraContainer.getInstance();
 
     private final String fileName;
 
-    public OrchestraCommand(String fileName) {
+    public OrchestraCommand(String fileName, Subsystem... requirements) {
         this.fileName = fileName;
+        addRequirements(requirements);
     }
 
     @Override
